e486e251272634b28c121306b9e20170d83fb0ba
neoastra303 <neozero3303@gmail.com>
2025-10-18 07:33:45 +0300

    up-commit

--- patients/management/commands/create_groups.py
+++ patients/management/commands/create_groups.py
@@ -11,5 +11,3 @@ class Command(BaseCommand):
             group, created = Group.objects.get_or_create(name=group_name)
             if created:
                 self.stdout.write(self.style.SUCCESS(f"Successfully created group: '{group_name}'"))
-            else:
-                self.stdout.write(self.style.WARNING(f"Group '{group_name}' already exists."))

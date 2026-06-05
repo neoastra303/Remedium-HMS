2d35ad84f6b523b2e0e4c35ec550729084c727bc
Neozero0 <neozero3303@gmail.com>
2026-06-05 22:34:54 +0300

    fix: add missing timezone import to patients/models.py
    - Add `from django.utils import timezone` import
    - Resolves flake8 F821 undefined name 'timezone' error
    - Prepares model for any timezone-dependent operations

--- patients/models.py
+++ patients/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 from datetime import date
 import re
 from simple_history.models import HistoricalRecords
